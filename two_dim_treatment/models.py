1ba56e5883805ee2747c8379812f374d8988d7d0
mlfreer <1991F@inbox.ru>
2021-02-02 22:02:56 +0000

    getting the interface going

--- two_dim_treatment/models.py
+++ two_dim_treatment/models.py
@@ -8,17 +8,44 @@ from otree.api import (
     Currency as c,
     currency_range,
 )
+from array import *
 
+import decimal
+import random
+
+author = 'Mikhail Freer'
 
 doc = """
-Your app description
+This is the treatment for two dimensional treatment of the qlt experiment, where subjects make decisions on the non-linear contracts.
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'two_dim_treatment'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 6
+
+    # maximum number of tasks:
+    num_tasks = 6
+
+    # number of budgets:
+    num_budgets = 15
+
+    # defining the budgets:
+    #wages = {}
+    #wages[1,1] = 4.02
+    #wages[1,2] = 6.51
+    #wages[1,3] = 8.09
+
+    wages = [[0 for x in range(0,6)]  for x in range(0,15)]
+    wages[0] = [4.02, 6.51, 8.09, 9.35, 10.34, 11.00]
+    wages[1] = [5.51, 7.26, 8.56, 9.39, 9.94, 10.33]
+    wages[2] = [5.76, 7.38, 8.37, 9.15, 9.71, 10.13]
+    wages[3] = [4.01, 6.51, 8.15, 9.37, 10.26, 10.93]
+    wages[4] = [5.02, 7.01, 8.40, 9.40, 10.18, 10.78]
+    wages[5] = [5.19, 7.10, 8.35, 9.25, 9.96, 10.41]
+
+    temp_wage = wages[1][2]
 
 
 class Subsession(BaseSubsession):
@@ -30,4 +57,8 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    choice = models.IntegerField(min=1,max=Constants.num_tasks)
+
+
+
+
--- two_dim_treatment/pages.py
+++ two_dim_treatment/pages.py
@@ -3,16 +3,48 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
+class ContractDecision(Page):
+	template_name ='two_dim_treatment/ContractDecision.html'
+	form_model = 'player'
+	form_fields = ['choice']
+
+	def vars_for_template(self):
+		i = self.player.subsession.round_number
+		w1 = Constants.wages[i-1][0]
+		w2 = Constants.wages[i-1][1]
+		w3 = Constants.wages[i-1][2]
+		w4 = Constants.wages[i-1][3]
+		w5 = Constants.wages[i-1][4]
+		w6 = Constants.wages[i-1][5]
+		return {
+				'w1': w1,
+				'w2': w2,
+				'w3': w3,
+				'w4': w4,
+				'w5': w5,
+				'w6': w6
+			}
+
+	#timeout_seconds = 180
+
+	#def before_next_page(player):
+	#	i = player.subsession.round_number
+		#player.wage1 = Constants.wages[i-1][0]
+
 
 
 class ResultsWaitPage(WaitPage):
-    pass
+	pass
+	#def after_all_players_arrive(group):
+		#p = group.get_players()
+		#p.get_wages()
 
 
 class Results(Page):
-    pass
+	def is_displayed(self):
+		return self.player.subsession.round_number == Constants.num_rounds
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+
+
+page_sequence = [ContractDecision, ResultsWaitPage, Results]
